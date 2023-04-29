e2cecb600b5751a6932c2a25ce26705c3006defc
q_h <q_h@inbox.ru>
2023-04-29 21:56:30 +0300

    chore: add demos

---
+++ examples/demo.cjs
@@ -0,0 +1,44 @@
+const { ffprobe } = require('../lib/index.js');
+const { createReadStream } = require('fs');
+// optional: specify the ffprobe path
+process.env.FFPROBE_PATH = require('ffprobe-static').path;
+// or
+// ffprobe.path = ffprobeStatic.path;
+
+// async/await
+async function run() {
+  try {
+    // file
+    const data = await ffprobe('../test/test file.mp4');
+    console.log(data.format.duration);
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    // URL
+    const { streams } = await ffprobe(
+      'https://github.com/kukhariev/ffprobe/raw/master/test/test%20file.mp4'
+    );
+    console.log(streams[0]?.duration);
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    // Readable Stream
+    const { format } = await ffprobe(createReadStream('../test/test file.mp4'));
+    console.log(format.duration);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+run();
+
+// node-style callback
+ffprobe('../test/test file.mp4', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.format.duration);
+  }
+});
