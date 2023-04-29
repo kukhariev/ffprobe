de151935e2a61135a3055b6e4e77f8dc8a0f37eb
q_h <q_h@inbox.ru>
2023-04-29 21:56:30 +0300

    chore: add demos

---
+++ examples/demo.ts
@@ -0,0 +1,45 @@
+import { createReadStream } from 'fs';
+import { ffprobe } from '../lib/index.js';
+// optional: specify the ffprobe path
+import { path as ffprobePath } from 'ffprobe-static';
+process.env.FFPROBE_PATH = ffprobePath;
+// or
+// ffprobe.path = ffprobePath;
+
+// async/await
+async function run() {
+  try {
+    // file
+    const data = await ffprobe('../test/test file.mp4');
+    console.log(data.format.duration);
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    // URL
+    const { streams } = await ffprobe(
+      'https://github.com/kukhariev/ffprobe/raw/master/test/test%20file.mp4'
+    );
+    console.log(streams[0]?.duration);
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    // Readable Stream
+    const { format } = await ffprobe(createReadStream('../test/test file.mp4'));
+    console.log(format.duration);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+run();
+
+// node-style callback
+ffprobe('../test/test file.mp4', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data?.format.duration);
+  }
+});
